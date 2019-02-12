7a4b069daf2c39f8f1faa903e57138a84d3d4aed
MiguelAntonioMaciasMeza <miguel6021857@gmail.com>
2019-02-12 10:33:13 -0800

    Allows user to record, but video files are unable to play

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
+import os           #To find out what files we are using 
 import numpy as np
 import cv2
 
-#cap = cv2.VideoCapture(0)
-filename = 'video.avi'
+filename = 'video.mp4'
 frames_per_seconds = 30;
-my_res = '4k'
+my_res = '720p'
 
 
 def change_res(cap,width, height):
@@ -25,12 +25,31 @@ def get_dims(cap, res='1080p'):
     change_res(cap, width, height)
     return width, height
 
+
+VIDEO_TYPE = {
+        'avi' : cv2.VideoWriter_fourcc(*'MPEG'),
+        'mp4' : cv2.VideoWriter_fourcc(*'MPEG'),
+}
+
+def get_video_type(filename):
+    filename, ext = os.path.splitext(filename)
+    if ext in VIDEO_TYPE:
+        return VIDEO_TYPE[ext]
+    return VIDEO_TYPE['avi']
+
+
+
 cap = cv2.VideoCapture(0)
 dims = get_dims(cap, res=my_res)
+video_type_cv2 = get_video_type(filename)
+
+out = cv2.VideoWriter(filename, video_type_cv2, frames_per_seconds, dims) #width, height
 
 while True:
      
+    #Provides frames 
     ret, frame = cap.read()
+    out.write(frame)
     cv2.imshow('Frame1', frame)    #Displays the feed
 
     if cv2.waitKey(2) & 0xFF == ord('q'):
@@ -38,4 +57,5 @@ while True:
 
 #Release the feed 
 cap.release()
+out.release()
 cv2.destroyAllWindows()
